7ce9964bc1af620fdc681345617c3ac5d346cc26
sango224 <sango.xx0224@gmail.com>
2024-03-17 13:30:57 +0900

    3/17

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var x = 10;
+var y = 5;
+if (x > y) {
+  console.log("xはyより大きい");
+}
+
+var x = 5;
+var y = 10;
+if (x < y) {
+  console.log("xはyより小さい");
+}
+
+var x = 10;
+var y = 5;
+if (x >= y) {
+  console.log("xはyより大きいまたは等しい");
+}
+
+var x = 5;
+var y = 10;
+if (x <= y) {
+  console.log("xはyより小さいまたは等しい");
+}
